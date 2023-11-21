4434cf511408b5f23476159ccb6817cf64197d86
Sanjarbek Mamayunusov <Sanjarbek@217-013.internet.ahi>
2023-11-22 00:12:36 +0500

    471 - Ход конем (__int128)

--- main.cpp
+++ main.cpp
@@ -2,31 +2,50 @@
 
 #include <bits/stdc++.h>
 #include <vector>
-//#define int long long
-   
-   
+// #define int long long
+
+using ll = __int128;
+	
 using namespace std;
-   
+
+
+ostream& operator<<(ostream& os, ll n){
+    string s = "";
+    do {
+        s = char(n % 10 + '0') + s;
+        n /= 10;
+    }while(n > 0);
+
+    return os << s;
+}
+
    
 void t_main(){
     int n;
-    cin>> n;
-    vector<int> a(n);
-     
-    for(int &x:a) cin >> x;
-     
-     
-    vector<vector<int> > dp(n+1, vector<int> (4, 0));
-     
-    dp[0][1] =  a[0];
-     
-    for(int i = 1; i < n+1; i++) {
-        dp[i][1] = dp[i-1][1] + a[i];
-        dp[i][2] = dp[i-1][2] + dp[i-1][1] * a[i];
-        dp[i][3] = dp[i-1][3] + dp[i-1][2] * a[i];
+    cin >> n;
+    vector<vector<ll>> dp(n+1, vector<ll> (10, 1));
+
+    dp[1][0] = dp[1][8] = 0;
+
+    for(int i = 2; i <= n; i++){
+            dp[i][0] = dp[i-1][4] + dp[i-1][6];
+            dp[i][1] = dp[i-1][8] + dp[i-1][6];
+            dp[i][2] = dp[i-1][7] + dp[i-1][9];
+            dp[i][3] = dp[i-1][4] + dp[i-1][8];
+            dp[i][4] = dp[i-1][0] + dp[i-1][3]+ dp[i-1][9];
+            dp[i][5] = 0;
+            dp[i][6] = dp[i-1][0] + dp[i-1][1]+ dp[i-1][7];
+            dp[i][7] = dp[i-1][2] + dp[i-1][6];
+            dp[i][8] = dp[i-1][1] + dp[i-1][3];
+            dp[i][9] = dp[i-1][2] + dp[i-1][4];
     }
-     
-    cout << dp[n-1][3];
+    ll s = 0;
+    for(int i = 0; i < 10; i++) s += dp[n][i];
+
+    cout << s;
+
+
+
 }
    
    
