66b04b311250dd14a0f775b8dfaada97f27a76f7
Sanjarbek Mamayunusov <Sanjarbek@217-013.internet.ahi>
2023-11-21 23:09:08 +0500

    525 - Сумма степеней двойки

--- main.cpp
+++ main.cpp
@@ -9,7 +9,17 @@ using namespace std;
    
    
 void t_main(){
-    
+    int n;
+    cin >> n;
+    vector<int> dp(n+1,0);
+
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i = 3; i <= n; i++){
+        dp[i] = dp[i-1] + (i % 2 == 0 ? dp[i/2] : 0);
+    }
+
+    cout << dp[n];
 }
    
    
