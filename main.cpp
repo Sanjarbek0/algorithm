620039cb638477f851005eaaa7a1b2a7aa0a9d47
Sanjarbek Mamayunusov <Sanjarbek@217-013.internet.ahi>
2023-11-21 23:14:55 +0500

    552 - Зоопарк

--- main.cpp
+++ main.cpp
@@ -10,16 +10,23 @@ using namespace std;
    
 void t_main(){
     int n;
-    cin >> n;
-    vector<int> dp(n+1,0);
-
-    dp[1] = 1;
-    dp[2] = 2;
-    for(int i = 3; i <= n; i++){
-        dp[i] = dp[i-1] + (i % 2 == 0 ? dp[i/2] : 0);
+    cin>> n;
+    vector<int> a(n);
+     
+    for(int &x:a) cin >> x;
+     
+     
+    vector<vector<int> > dp(n+1, vector<int> (4, 0));
+     
+    dp[0][1] =  a[0];
+     
+    for(int i = 1; i < n+1; i++) {
+        dp[i][1] = dp[i-1][1] + a[i];
+        dp[i][2] = dp[i-1][2] + dp[i-1][1] * a[i];
+        dp[i][3] = dp[i-1][3] + dp[i-1][2] * a[i];
     }
-
-    cout << dp[n];
+     
+    cout << dp[n-1][3];
 }
    
    
